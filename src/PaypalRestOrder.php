0d17e9e065b4d62915ab87f769857cf3712c34ee
Jochen Schultz <js@km-it.de>
2015-06-16 16:53:34 +0200

    switch different payment states

--- src/PaypalRestOrder.php
+++ src/PaypalRestOrder.php
@@ -14,7 +14,7 @@ use PayPal\Api\PaymentExecution;
 
 class PaypalRestOrder {
 
-	protected function create_item_list($order) {
+	private function create_item_list($order) {
 		foreach($order->products as $product) {
 			if((trim($product['qty'])!='')) {
 				$item = new Item();
@@ -37,7 +37,7 @@ class PaypalRestOrder {
 		return $itemList;
 	}
 	
-	protected function calculate_total() {
+	private function calculate_total() {
 		$this->subtotal = 0;
 		for ($i=0,$cnt = count($this->item);$i<$cnt;$i++) {
 			$this->subtotal += ($this->item[$i]->price * $this->item[$i]->quantity);
@@ -45,19 +45,19 @@ class PaypalRestOrder {
 		$this->total = $this->subtotal;
 	}
 	
-	protected function set_details() {
+	private function set_details() {
 		$this->details = new Details();
 		$this->details->setShipping(0)->setTax(0)->setSubtotal($this->subtotal);
 	}
 	
-	protected function set_transaction($order,$orderdesc,$custom,$itemList) {
+	private function set_transaction($order,$orderdesc,$custom,$itemList) {
 		$amount = new Amount();
 		$amount->setCurrency($order->info['currency'])->setTotal($this->total)->setDetails($this->details);
 		$this->transaction = new Transaction();
 		$this->transaction->setAmount($amount)->setItemList($itemList)->setDescription($orderdesc)->setInvoiceNumber($custom);
 	}
 	
-	protected function save_order($order,$custom,$paymentid) {
+	private function save_order($order,$custom,$paymentid) {
 		$SQL ="UPDATE warenkoerbe SET paypal_token = '".$paymentid."' WHERE paypal_token='".$custom."'";
 		$this->db->db_query($SQL,__FILE__,__LINE__);
 		$SQL ="UPDATE warenkorbhistory SET paypal_token = '".$paymentid."' WHERE paypal_token='".$custom."'";
@@ -88,18 +88,74 @@ class PaypalRestOrder {
 		return $payment;
     }
 	
-	public function execute_payment() {
+	/**
+	 * 
+	 * @param type $order
+	 */
+	private function payment_approved($order,$paymentId) {
+		$order->info['payment_status'] = 'Completed'; // die validate-Klasse hat ihr OK gegeben
+		$order->txn_id = $paymentId;
+		$order->save();
+		return 'completed';
+	}
+	
+	private function payment_created() { return 'fup2devmail'; }
+	private function payment_failed() { return 'overview'; }
+	private function payment_canceled() { return 'overview'; }
+	private function payment_expired() { return 'overview'; }
+	private function payment_pending() { return 'fup2mail'; }
+	private function payment_in_progress() { return 'fup2mail'; }
+	private function payment_not_implemented() { return 'fup2devmail'; }
+
+	private function check_payment_state($executedpayment,$order,$paymentId) {
+		switch($executedpayment->state) {
+			case('created'):		return $this->payment_created();
+				break;
+			case('approved'):		return $this->payment_approved($order,$paymentId);
+				break;
+			case('failed'):			return $this->payment_failed();
+				break;
+			case('canceled'):		return $this->payment_canceled();
+				break;
+			case('expired'):		return $this->payment_expired();
+				break;
+			case('pending'):		return $this->payment_pending();
+				break;
+			case('in_progress'):	return $this->payment_in_progress();
+				break;
+			default:				return $this->payment_not_implemented();
+		}
+	}
+	
+	/**
+	 * recieve paymentinfo via givven $paymentId (customid/paypal_token)
+	 * 
+	 * @param type $paymentId
+	 * @return obj $payment
+	 */
+	public function get_payment($paymentId) {
+		try {
+			$payment = Payment::get($paymentId, $this->PaypalApicontext);
+		} catch (Exception $ex) {
+			// NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+			\Debugteam\Baselib\ResultPrinter::printError("Get Payment", "Payment", null, null, $ex);
+			exit(1);
+		}
+		// NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+		 \Debugteam\Baselib\ResultPrinter::printResult("Get Payment", "Payment", $payment->getId(), null, $payment);
+		return $payment;		
+	}
+	
+	public function execute_payment($order) {
 		$paymentId = $_GET['paymentId'];
 		$payment = Payment::get($paymentId, $this->PaypalApicontext);	
 		$execution = new PaymentExecution();
 		$execution->setPayerId($_GET['PayerID']);
-		
-		
 		try {
 			$result = $payment->execute($execution, $this->PaypalApicontext);
 			#\Debugteam\Baselib\ResultPrinter::printResult("Executed Payment", "Payment", $payment->getId(), $execution, $result);
 	        try {
-		        $payment = Payment::get($paymentId, $this->PaypalApicontext);
+		        $executedpayment = Payment::get($paymentId, $this->PaypalApicontext);
 			} catch (Exception $ex) {
 				#\Debugteam\Baselib\ResultPrinter::printError("Get Payment", "Payment", null, null, $ex);
 				exit('failed');
@@ -111,7 +167,10 @@ class PaypalRestOrder {
 		}		
 		#\Debugteam\Baselib\ResultPrinter::printResult("Get Payment", "Payment", $payment->getId(), null, $payment);
 			
-		return $payment;			
+		$paymentcompleted = new \stdClass();
+		$paymentcompleted->next = $this->check_payment_state($executedpayment,$order,$paymentId);
+		$paymentcompleted->payment = $executedpayment;
+		return $paymentcompleted;
 	}
 	
 	public function failed_payment() {
