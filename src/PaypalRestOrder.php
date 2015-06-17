8ccbc352fbb8581e02b223c5bb90d72302bf1db8
Jochen Schultz <js@km-it.de>
2015-06-17 17:23:12 +0200

    use payer data in execute payment

--- src/PaypalRestOrder.php
+++ src/PaypalRestOrder.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * change this file with pull request to https://github.com/debugteam/paypalrestorder
- * 
+ *
  */
 
 namespace Debugteam\Paypalrest;
@@ -29,7 +29,7 @@ class PaypalRestOrder {
 		if ($order->getBearbeitungsgebuehr()) {
 			$item = new Item();
 			$item->setName('Bearbeitungsgebühren')->setCurrency($order->info['currency'])->setQuantity(1)->setSku(9999)->setPrice(number_format($order->getBearbeitungsgebuehr(),2,'.',''));
-			$this->item[] = $item;			
+			$this->item[] = $item;
 		}
 		if ($order->getActioncodeDiscount()) {
 			$item = new Item();
@@ -40,7 +40,7 @@ class PaypalRestOrder {
 		$itemList->setItems($this->item);
 		return $itemList;
 	}
-	
+
 	private function calculate_total() {
 		$this->subtotal = 0;
 		for ($i=0,$cnt = count($this->item);$i<$cnt;$i++) {
@@ -48,7 +48,7 @@ class PaypalRestOrder {
 		}
 		$this->total = $this->subtotal;
 	}
-	
+
 	private function set_details() {
 		$this->details = new Details();
 		$this->details->setShipping(0)->setTax(0)->setSubtotal($this->subtotal);
@@ -85,56 +85,124 @@ class PaypalRestOrder {
 			$payment->create($this->PaypalApicontext);
 		} catch (Exception $ex) {
 		//	\Debugteam\Baselib\ResultPrinter::printError("Created Payment Using PayPal. Please visit the URL to Approve.", "Payment", null, $request, $ex);
-			//trigger_error('Paypal Zahlung fehlgeschlagen! Achtung! Dies ist keine Übung!!!');
-		}	
+			trigger_error('Paypal Zahlung fehlgeschlagen!'.$ex->getMessage().$ex->getTraceAsString().' triggered in line:'.$ex->getLine());
+		}
 		//$approvalUrl = $payment->getApprovalLink();
 		//\Debugteam\Baselib\ResultPrinter::printResult("Created Payment Using PayPal. Please visit the URL to Approve.", "Payment", "<a href='$approvalUrl' >$approvalUrl</a>", $request, $payment);
 		$this->save_order($order,$custom,$payment->id);
 		return $payment;
     }
 
+	private function payment_save_completed($order,$executedpayment) {
+		$order->info['payment_status'] = 'Completed';
+		$order->txn_id = $executedpayment->transactions[0]->related_resources[0]->sale->id;
+		$order->customer['first_name']		=  $executedpayment->payer->payer_info->first_name;
+		$order->customer['last_name']		=  $executedpayment->payer->payer_info->last_name;
+		$order->customer['email_address']	=  $executedpayment->payer->payer_info->email;
+		//$order->customer['phone']			=  $this->keyarray['contact_phone'];
+		$order->customer['phone']			=  '';
+		$order->customer['address_street']	=  $executedpayment->payer->payer_info->shipping_address->line1;
+		$order->customer['address_zip']		=  $executedpayment->payer->payer_info->shipping_address->postal_code;
+		$order->customer['address_city']	=  $executedpayment->payer->payer_info->shipping_address->city;
+		$order->customer['address_country']	=  $executedpayment->payer->payer_info->shipping_address->country_code;
+		$order->save();
+	}
+
+	private function payment_save_pending($order,$executedpayment) {
+		$order->info['payment_status'] = 'Pending';
+		$order->txn_id = $executedpayment->transactions[0]->related_resources[0]->sale->id;
+		$order->customer['first_name']		=  $executedpayment->payer->payer_info->first_name;
+		$order->customer['last_name']		=  $executedpayment->payer->payer_info->last_name;
+		$order->customer['email_address']	=  $executedpayment->payer->payer_info->email;
+		//$order->customer['phone']			=  $this->keyarray['contact_phone'];
+		$order->customer['phone']			=  '';
+		$order->customer['address_street']	=  $executedpayment->payer->payer_info->shipping_address->line1;
+		$order->customer['address_zip']		=  $executedpayment->payer->payer_info->shipping_address->postal_code;
+		$order->customer['address_city']	=  $executedpayment->payer->payer_info->shipping_address->city;
+		$order->customer['address_country']	=  $executedpayment->payer->payer_info->shipping_address->country_code;
+		$order->save();
+	}
+
+	private function payment_save_observation($order,$executedpayment) {
+		$order->info['payment_status'] = 'Observation';
+		$order->txn_id = $executedpayment->transactions[0]->related_resources[0]->sale->id;
+		$order->customer['first_name']		=  $executedpayment->payer->payer_info->first_name;
+		$order->customer['last_name']		=  $executedpayment->payer->payer_info->last_name;
+		$order->customer['email_address']	=  $executedpayment->payer->payer_info->email;
+		//$order->customer['phone']			=  $this->keyarray['contact_phone'];
+		$order->customer['phone']			=  '';
+		$order->customer['address_street']	=  $executedpayment->payer->payer_info->shipping_address->line1;
+		$order->customer['address_zip']		=  $executedpayment->payer->payer_info->shipping_address->postal_code;
+		$order->customer['address_city']	=  $executedpayment->payer->payer_info->shipping_address->city;
+		$order->customer['address_country']	=  $executedpayment->payer->payer_info->shipping_address->country_code;
+		$order->paypalpayment				=  $executedpayment;
+		$order->save();
+	}
+
 	/**
-	 * 
+	 *
 	 * @param type $order
 	 */
-	private function payment_approved($order,$paymentId) {
-		$order->info['payment_status'] = 'Completed'; // die validate-Klasse hat ihr OK gegeben
-		$order->txn_id = $paymentId;
-		$order->save();
+	private function payment_approved($order,$executedpayment) {
+		$this->payment_save_completed($order,$executedpayment);
 		return 'completed';
 	}
-	
-	private function payment_created() { return 'fup2devmail'; }
-	private function payment_failed() { return 'overview'; }
-	private function payment_canceled() { return 'overview'; }
-	private function payment_expired() { return 'overview'; }
-	private function payment_pending() { return 'fup2mail'; }
-	private function payment_in_progress() { return 'fup2mail'; }
-	private function payment_not_implemented() { return 'fup2devmail'; }
 
-	private function check_payment_state($executedpayment,$order,$paymentId) {
-		switch($executedpayment->state) {
-			case('created'):		return $this->payment_created();
-				break;
-			case('approved'):		return $this->payment_approved($order,$paymentId);
-				break;
-			case('failed'):			return $this->payment_failed();
-				break;
-			case('canceled'):		return $this->payment_canceled();
-				break;
-			case('expired'):		return $this->payment_expired();
-				break;
-			case('pending'):		return $this->payment_pending();
-				break;
-			case('in_progress'):	return $this->payment_in_progress();
-				break;
-			default:				return $this->payment_not_implemented();
-		}
+	private function payment_created($order,$executedpayment) {
+		$this->payment_save_observation($order,$executedpayment);
+		return 'fup2devmail';
+	}
+
+	private function payment_failed($order,$executedpayment) {
+		return 'overview';
+	}
+
+	private function payment_canceled($order,$executedpayment) {
+		return 'overview';
 	}
 	
-	/** -- currently unused 
+	private function payment_expired($order,$executedpayment) {
+		return 'overview';
+	}
+
+	private function payment_pending($order,$executedpayment) {
+		$this->payment_save_pending($order,$executedpayment);
+		return 'fup2mail';
+	}
+
+	private function payment_in_progress($order,$executedpayment) {
+		$this->payment_save_pending($order,$executedpayment);
+		return 'fup2mail';
+	}
+
+	private function payment_not_implemented($order,$executedpayment) {
+		$this->payment_save_observation($order,$executedpayment);
+		return 'fup2devmail';
+	}
+
+	private function check_payment_state($executedpayment,$order) {
+		switch($executedpayment->state) {
+			case('created'):		return $this->payment_created($order,$executedpayment);
+				break;
+			case('approved'):		return $this->payment_approved($order,$executedpayment);
+				break;
+			case('failed'):			return $this->payment_failed($order,$executedpayment);
+				break;
+			case('canceled'):		return $this->payment_canceled($order,$executedpayment);
+				break;
+			case('expired'):		return $this->payment_expired($order,$executedpayment);
+				break;
+			case('pending'):		return $this->payment_pending($order,$executedpayment);
+				break;
+			case('in_progress'):	return $this->payment_in_progress($order,$executedpayment);
+				break;
+			default:				return $this->payment_not_implemented($order,$executedpayment);
+		}
+	}
+
+	/** -- currently unused
 	 * recieve paymentinfo via givven $paymentId (customid/paypal_token)
-	 * 
+	 *
 	 * @param type $paymentId
 	 * @return obj $payment
 	 */
@@ -148,12 +216,12 @@ class PaypalRestOrder {
 		}
 		// NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
 		 \Debugteam\Baselib\ResultPrinter::printResult("Get Payment", "Payment", $payment->getId(), null, $payment);
-		return $payment;		
+		return $payment;
 	}
-	
+
 	public function execute_payment($order) {
 		$paymentId = $_GET['paymentId'];
-		$payment = Payment::get($paymentId, $this->PaypalApicontext);	
+		$payment = Payment::get($paymentId, $this->PaypalApicontext);
 		$execution = new PaymentExecution();
 		$execution->setPayerId($_GET['PayerID']);
 		try {
@@ -166,32 +234,22 @@ class PaypalRestOrder {
 				exit('failed');
 				// foo
 			}
-		} catch (Exception $ex) {		
+		} catch (Exception $ex) {
 			#\Debugteam\Baselib\ResultPrinter::printError("Executed Payment", "Payment", null, null, $ex);
 			exit('failed');
-		}		
+		}
 		#\Debugteam\Baselib\ResultPrinter::printResult("Get Payment", "Payment", $payment->getId(), null, $payment);
 		$paymentcompleted = new \stdClass();
-		$paymentcompleted->next = $this->check_payment_state($executedpayment,$order,$paymentId);
+		$paymentcompleted->next = $this->check_payment_state($executedpayment,$order);
 		$paymentcompleted->payment = $executedpayment;
-		$order->customer['first_name']		=  $executedpayment->payer->payer_info->first_name;
-		$order->customer['last_name']		=  $executedpayment->payer->payer_info->last_name;
-		$order->customer['email_address']	=  $executedpayment->payer->payer_info->email;
-		//$order->customer['phone']			=  $this->keyarray['contact_phone'];
-		$order->customer['phone']			=  '';
-		$order->customer['address_street']	=  $executedpayment->payer->payer_info->shipping_address->line1;
-		$order->customer['address_zip']		=  $executedpayment->payer->payer_info->shipping_address->postal_code;
-		$order->customer['address_city']	=  $executedpayment->payer->payer_info->shipping_address->city;
-		$order->customer['address_country']	=  $executedpayment->payer->payer_info->shipping_address->country_code;		
-		$order->save();
 		return $paymentcompleted;
 	}
-	
+
 	public function failed_payment() {
 		\Debugteam\Baselib\ResultPrinter::printResult("User Cancelled the Approval", null);
-		exit;			
+		exit;
 	}
-	
+
     public function __construct($clientId='',$clientSecret='') {
 		if (empty($clientId)||empty($clientSecret)) {
 			trigger_error('Hey developer (future me): you need to provide credentials to use the Paypal Rest Api! Go get them here: https://developer.paypal.com/webapps/developer/applications/myapps');
@@ -199,7 +257,7 @@ class PaypalRestOrder {
 		}
 		$this->PaypalApicontext = \Debugteam\Baselib\PaypalHelper::getApiContext($clientId, $clientSecret);
 		$this->redirectUrls = new RedirectUrls();
-		$this->redirectUrls->setReturnUrl(BASE_URL."danke.php?action=succeededpayment")->setCancelUrl(BASE_URL."overview.php?&action=cancledpayment");		
+		$this->redirectUrls->setReturnUrl(BASE_URL."danke.php?action=succeededpayment")->setCancelUrl(BASE_URL."overview.php?&action=cancledpayment");
 
 		$this->db = new \Debugteam\Baselib\Db;
 		if (!$this->db->is_open_con()) {
